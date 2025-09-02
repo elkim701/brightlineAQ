9b8298ee4658a315ab3ebe34e9763cf0c047117e
elkim701 <eleanor.kim701@berkeley.edu>
2025-09-02 15:53:10 -0700

    update latest files

--- Programs/Clean_BC_raw.R
+++ Programs/Clean_BC_raw.R
@@ -2,8 +2,8 @@
 # Title: Data Cleaning for Raw BC Clarity Data
 # Author: Eleanor Kim
 # Last Updated: 21 March 2025
-# Description: This script performs data cleaning for raw Clarity data, 
-#              including the addition of device names and neighborhood 
+# Description: This script performs data cleaning for raw Clarity data,
+#              including the addition of device names and neighborhood
 #              classifications, and preparing the data for further analysis.
 # ------------------------------------------------------------------------------
 
@@ -13,18 +13,48 @@ library(lubridate)
 
 # Set-up
 dir <- "/Users/johnkim/Desktop/brightlineAQ/"
+#dir <- "C:/Users/Eleanor-Kim/OneDrive - MMC/Documents/brightlineAQ/"
 
 
 # Read in raw files from Clarity
-new_data0 <- read.csv(paste0(dir,"RAW/BC_21mar25_30apr25.csv"))
-old_data0 <- read.csv(paste0(dir,"CLEAN/cleanBC_12dec24_21mar25.csv"))[,2:20] # rm index col
+new_data0 <- read.csv(paste0(dir,"RAW/BC_7jul25_22aug25.csv"))
+old_data0 <- read.csv(paste0(dir,"CLEAN/cleanBC_12dec24_7jul25.csv"))[,2:20] # rm index col
+max(old_data0$Datetime)
+
+# clean up old data device names
+old_data0 <- old_data0 %>%
+  mutate(Device_Name = case_when(
+    alt.ID == "DVCKP7679" ~ "Carroll Ave. & 3rd St. (BC)", # bvhp foundation
+    alt.ID == "DPANQ2934" ~ "Air District Reference Site",
+    alt.ID == "DNHZP3586" ~ "9th St. and Brannan St. (BC)", # fitness sf
+    alt.ID == "DGVIU8498" ~ "Kirkwood Ave. & Earl St. (BC)",
+    alt.ID == "DBUFQ1648" ~ "Newcomb Ave. & 3rd St. (BC)",
+    alt.ID == "DEVXA9067" ~ "Hyde St. & Golden Gate Ave. (BC)",
+    alt.ID == "DCDBK0901" ~ "Howard St. & 6th St. (BC)",
+    alt.ID == "DSARJ4044" ~ "Eddy St. & Jones St. (BC)",
+    alt.ID == "DAJXS2653" ~ "Clay St. & Kearny St. (BC)",
+    alt.ID == "DFPAI0612" ~ "Broadway St. & Stockton St. (BC)",
+    alt.ID == "DSUSD5545" ~ "Broadway St. & Stockton St. (BC)",
+    
+    TRUE ~ Device_Name
+  ))
+table(old_data0$Neighborhood)
+old_data0 <- old_data0 %>%
+  mutate(Neighborhood = case_when(
+    alt.ID %in% c("DVCKP7679","DGVIU8498") ~ "BVHP",
+    alt.ID %in% c("DPANQ2934", "DBUFQ1648") ~ "Potrero Hill",
+    alt.ID %in% c("DNHZP3586", "DCDBK0901") ~ "SoMa",
+    alt.ID %in% c("DSARJ4044" ,"DEVXA9067") ~ "Tenderloin",
+    alt.ID %in% c("DAJXS2653","DFPAI0612","DSUSD5545" ) ~ "Chinatown",
+    TRUE ~ Neighborhood
+  ))
 
 # Drop unnecessary columns and select relevant ones
 data <- new_data0 %>%
   select(contains("sourceId") |
            contains("location") |
            (contains("bc") & contains("1HourMean.value")) |# which bc fields to keep?
-           contains("start") | 
+           contains("start") |
            contains("temperatureInternal1HourMean.value") | # any other atmospheric data to keep?
            contains("relHumidInternal1HourMean.value") |
            contains("pm2_5ConcMass1HourMean.value") | # which pm2.5 fields to keep?
@@ -40,27 +70,27 @@ old_data0$Datetime <- ymd_hms(old_data0$Datetime, tz = Sys.timezone())
 # Create a column for Neighborhood
 data <- data %>%
   mutate(Neighborhood = case_when(
-    datasourceId %in% c("DVCKP7679","DGVIU8498") ~ "BVHP",
+    datasourceId %in% c("DVCKP7679","DGVIU8498","DSUSD5545") ~ "BVHP",
     datasourceId %in% c("DPANQ2934", "DBUFQ1648") ~ "Potrero Hill",
     datasourceId %in% c("DNHZP3586", "DCDBK0901") ~ "SoMa",
     datasourceId %in% c("DSARJ4044" ,"DEVXA9067") ~ "Tenderloin",
     datasourceId %in% c("DAJXS2653","DFPAI0612" ) ~ "Chinatown",
   ))
-    
+
 
 # Create a column for Device Name
 data <- data %>%
   mutate(Device_Name = case_when(
-    datasourceId == "DVCKP7679" ~ "BVHP Foundation",
+    datasourceId == "DVCKP7679" ~ "Carroll Ave. & 3rd St. (BC)", # bvhp foundation
     datasourceId == "DPANQ2934" ~ "Air District Reference Site",
-    datasourceId == "DNHZP3586" ~ "Fitness SF SOMA",
-    datasourceId == "DGVIU8498" ~ "Wu Yee Kirkwood",
-    datasourceId == "DBUFQ1648" ~ "Joseph Lee Rec Center",
-    datasourceId == "DEVXA9067" ~ "126 Hyde St",
-    datasourceId == "DCDBK0901" ~ "United Playaz",
-    datasourceId == "DSARJ4044" ~ "Drake Hotel",
-    datasourceId == "DAJXS2653" ~ "665 Clay St",
-    datasourceId == "DFPAI0612" ~ "Bayside Elderly"
+    datasourceId == "DNHZP3586" ~ "9th St. and Brannan St. (BC)", # fitness sf
+    datasourceId == "DGVIU8498" ~ "Kirkwood Ave. & Earl St. (BC)",
+    datasourceId == "DBUFQ1648" ~ "Newcomb Ave. & 3rd St. (BC)",
+    datasourceId == "DEVXA9067" ~ "Hyde St. & Golden Gate Ave. (BC)",
+    datasourceId == "DCDBK0901" ~ "Howard St. & 6th St. (BC)",
+    datasourceId == "DSARJ4044" ~ "Eddy St. & Jones St. (BC)",
+    datasourceId == "DAJXS2653" ~ "Clay St. & Kearny St. (BC)",
+    datasourceId == "DFPAI0612" ~ "Broadway St. & Stockton St. (BC)"
   ))
 
 # Rename columns
@@ -68,7 +98,7 @@ names(data) = c('alt.ID','ID','Latitude','Longitude',
                 'BC_AllSources_Hour_Calibrated','BC_Biomass_Hour_Calibrated','BC_FossilFuel_Hour_Calibrated',
                 'BC_SpectralB1_Hour_Calibrated','BC_SpectralG1_Hour_Calibrated','BC_SpectralIR1_Hour_Calibrated',
                 'BC_SpectralR1_Hour_Calibrated','BC_SpectralUV1_Hour_Calibrated',
-                'Datetime', 'Temperature','Humidity','PM2.5_Hour_MassConc_Calibrated', 'NO2_Hour_MassConc_Calibrated', 
+                'Datetime', 'Temperature','Humidity','PM2.5_Hour_MassConc_Calibrated', 'NO2_Hour_MassConc_Calibrated',
                 'Neighborhood','Device_Name')
 
 # Check data
@@ -79,28 +109,4 @@ combined <- rbind(old_data0, data)
 updated <- combined[!duplicated(combined), ] # drop dups if any
 
 # Save to csv
-write.csv(updated,paste0(dir,"CLEAN/cleanBC_12dec24_21mar25.csv"))
-
-
-# combine with old PM data
-old_dataPM <- read.csv(paste0(dir,"CLEAN/cleanPM_19aug20_31dec24.csv"))[,2:12] # rm index col
-dataPM <- old_dataPM %>%
-  mutate(
-    BC_AllSources_Hour_Calibrated = NA,
-    BC_Biomass_Hour_Calibrated = NA,
-    BC_FossilFuel_Hour_Calibrated = NA,
-    BC_SpectralB1_Hour_Calibrated = NA,
-    BC_SpectralG1_Hour_Calibrated = NA,
-    BC_SpectralIR1_Hour_Calibrated = NA,
-    BC_SpectralR1_Hour_Calibrated = NA,
-    BC_SpectralUV1_Hour_Calibrated = NA
-  ) %>% select(names(updated))
-dataPM$Datetime <- ymd_hms(dataPM$Datetime, tz = Sys.timezone())
-
-combined2 <- rbind(dataPM, updated)
-updated2 <- combined2[!duplicated(combined2), ]
-updated2$Datetime <- ymd_hms(updated2$Datetime, tz = Sys.timezone())
-max(updated2$Datetime)
-write.csv(updated2,paste0(dir,"CLEAN/cleanBCPM_19aug20_30apr25.csv"))
-tmp <- read.csv(paste0(dir,"CLEAN/cleanBCPM_19aug20_21mar25.csv"))
-max(tmp$Datetime)
+write.csv(updated,paste0(dir,"CLEAN/cleanBC_use.csv"))
--- Programs/Clean_Seg_raw.R
+++ Programs/Clean_Seg_raw.R
@@ -2,8 +2,8 @@
 # Title: Data Cleaning Traffic Data
 # Author: Eleanor Kim
 # Last Updated: 10 June 2025
-# Description: This script performs data cleaning for raw Clarity data, 
-#              including the addition of device names and neighborhood 
+# Description: This script performs data cleaning for raw Clarity data,
+#              including the addition of device names and neighborhood
 #              classifications, and preparing the data for further analysis.
 # ------------------------------------------------------------------------------
 
@@ -16,6 +16,7 @@ library(lubridate)
 
 # Set-up
 dir <- "/Users/johnkim/Desktop/brightlineAQ/"
+dir <- "C:/Users/Eleanor-Kim/OneDrive - MMC/Documents/Projects/Brightline/brightlineAQ/"
 
 
 # Read in raw files from Clarity
@@ -23,6 +24,22 @@ seg_sensor <- read.csv(paste0(dir,"RAW/closest_segments_results.csv")) # 60 x 8 
 avg_speed_block <- read.csv(paste0(dir,"RAW/speed_by_hourblock.csv")) # 11575 x 4 segment-month-time of day
 avg_speed_hour <- read.csv(paste0(dir,"RAW/filtered_file.csv")) # 68686 x 11 segment-month-hour
 seg_loc <- read.csv(paste0(dir,"RAW/sfcta_cmp_exp_segments - sfcta_cmp_exp_segments.csv")) # 740 x 7 segment
+aq <- read.csv(paste0(dir,"CLEAN/cleanBCPM_19aug20_30apr25.csv")) # 740 x 7 segment
+
+# scrub aq data
+aq_filtered <- aq %>%
+  filter(grepl("2025-03", Datetime) & BC_AllSources_Hour_Calibrated>0) %>% # March analysis only
+  select(Datetime, ID, Device_Name, Latitude, Longitude, BC_AllSources_Hour_Calibrated) %>%
+  mutate(
+    Datetime = ymd_hms(Datetime),  # Convert to datetime format
+    Hour = hour(Datetime),          # Extract hour
+    Month = month(Datetime),        # Extract month
+    Year = year(Datetime)           # Extract year
+  ) %>%
+  group_by(Year, Month, Hour, ID, Device_Name, Latitude, Longitude) %>%
+  summarize(Average_BC = mean(BC_AllSources_Hour_Calibrated, na.rm = TRUE), .groups = 'drop')
+names(aq_filtered) = tolower(names(aq_filtered))
+
 
 # Function to extract coords in appropriate format
 extract_coords <- function(geojson_str) {
@@ -31,9 +48,9 @@ extract_coords <- function(geojson_str) {
   if (is.matrix(coords)) {
     coords <- split(coords, seq(nrow(coords)))
   }
-  return(coords)
-  
-  # Apply transformation
+  return(coords)}
+
+# Apply transformation
 seg_loc_long <- seg_loc %>%
   mutate(coords = map(geometry, extract_coords)) %>%
   unnest(coords) %>%
@@ -44,10 +61,13 @@ seg_loc_long <- seg_loc %>%
   group_by(cmp_segid) %>%
   mutate(point_index = row_number()) %>%
   ungroup() %>%
-  select(cmp_segid, point_index, longitude, latitude, cmp_name, direction, length)
+  select(cmp_segid, point_index, longitude, latitude, cmp_name, direction, length) %>% filter(latitude>0)
 
-# Merge with other data
-x = merge(seg_loc_long, avg_speed_hour %>% filter(date == "2025-03-01"), by = "cmp_segid", all.x = TRUE, all.y = TRUE)
+# Merge geo segments with speed data
+segments = merge(seg_loc_long, avg_speed_hour %>% filter(date == "2025-03-01"), by = "cmp_segid", all.x = TRUE, all.y = TRUE)
+segments2 <- segments %>% mutate(month = 3, hour = period) %>% select(-period, -date)
 
-write.csv(x,paste0(dir,"CLEAN/xtra_long_sfcta_seg.csv"))
+# Merge segments with air quality# Merge segments with air qualselect()ity
+aq_segments <- merge(segments2, aq_filtered, by = c('latitude','longitude', 'year', 'month', 'hour'), all=TRUE)
 
+write.csv(aq_segments,paste0(dir,"CLEAN/march_bc_sfcta_seg.csv"))
